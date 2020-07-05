ad2499a531e01245f8deffd64e1df3cc5c57cad2
Emil <emilrakh@gmail.com>
2020-07-05 18:13:00 +0500

    Smth

--- src/main/java/server/NexignServerAPI/ServerApiApplication.java
+++ src/main/java/server/NexignServerAPI/ServerApiApplication.java
@@ -1,4 +1,4 @@
-package server.ServerAPI;
+package server.NexignServerAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
