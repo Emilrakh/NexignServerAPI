98c1f0cc429d85537313523877e4e11d342bd49c
Emil <emilrakh@gmail.com>
2020-07-07 18:32:16 +0500

    AddService

--- src/main/java/server/NexignServerAPI/model/User.java
+++ src/main/java/server/NexignServerAPI/model/User.java
@@ -1,29 +1,30 @@
 package server.NexignServerAPI.model;
 
 import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Data
+@AllArgsConstructor
 @Table(name = "Users")
 public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userID;
-
     private String name;
-
     private String email;
-
     private String status;
     @Basic
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date actionDate = new Date();
 
-    public User() {
+    public User(User user) {
         super();
     }
 
@@ -39,46 +40,6 @@ public class User{
         this.email = email;
         this.status = status;
         this.actionDate = actionDate;
-    }
-
-    public void setUserID(long userID) {
-        this.userID = userID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setActionDate(java.util.Date actionDate) {
-        this.actionDate = actionDate;
-    }
-
-    public long getUserID() {
-        return userID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public java.util.Date getActionDate() {
-        return actionDate;
     }
 
     @Override
